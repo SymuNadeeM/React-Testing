da6839337f050c118b86d05c56d37d39b19bc8b9
SymuNadeem <msymunnadeem@gmail.com>
2024-04-22 21:00:57 +0600

    input test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-       <p>Hello Bangladesh</p>
+      <p>Hello Bangladesh</p>
+      <input
+        type="text"
+        placeholder="Type Something"
+        name="username"
+        id="userId"
+        value="kamal"
+      />
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,15 @@ test('hello bd', () => {
   const text = screen.getByText(/Hello Bangladesh/i);
   expect(text).toBeInTheDocument();
 });
+
+test('for text-box', () => {
+  render(<App />);
+  let inputBox = screen.getByRole("textbox")
+  let inputPlaceHolder = screen.getByPlaceholderText("Type Something")
+  expect(inputBox).toBeInTheDocument()       
+  expect(inputPlaceHolder).toBeInTheDocument()    
+  expect(inputBox).toHaveAttribute('name',"username")  
+  expect(inputBox).toHaveAttribute('id',"userId")  
+  expect(inputBox).toHaveAttribute('type',"text")   
+  expect(inputBox).toHaveAttribute('value',"kamal")   
+});
