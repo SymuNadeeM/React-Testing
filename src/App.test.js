9d5517510afaa47c41765de5bd87f9b33f129f02
SymuNadeem <msymunnadeem@gmail.com>
2024-04-23 12:37:10 +0600

    group

--- src/App.test.js
+++ src/App.test.js
@@ -1,20 +1,57 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('hello bd', () => {
+test("hello bd", () => {
   render(<App />);
   const text = screen.getByText(/Hello Bangladesh/i);
   expect(text).toBeInTheDocument();
 });
 
-test('for text-box', () => {
+test("for text-box", () => {
   render(<App />);
-  let inputBox = screen.getByRole("textbox")
-  let inputPlaceHolder = screen.getByPlaceholderText("Type Something")
-  expect(inputBox).toBeInTheDocument()       
-  expect(inputPlaceHolder).toBeInTheDocument()    
-  expect(inputBox).toHaveAttribute('name',"username")  
-  expect(inputBox).toHaveAttribute('id',"userId")  
-  expect(inputBox).toHaveAttribute('type',"text")   
-  expect(inputBox).toHaveAttribute('value',"kamal")   
+  let inputBox = screen.getByRole("textbox");
+  let inputPlaceHolder = screen.getByPlaceholderText("Type Something");
+  expect(inputBox).toBeInTheDocument();
+  expect(inputPlaceHolder).toBeInTheDocument();
+  expect(inputBox).toHaveAttribute("name", "username");
+  expect(inputBox).toHaveAttribute("id", "userId");
+  expect(inputBox).toHaveAttribute("type", "text");
+  expect(inputBox).toHaveAttribute("value", "kamal");
+});
+
+// group test-----------------------------------
+
+describe("UI test case group", () => {
+  test("case 1", () => {
+    render(<App />);
+    let inputBox = screen.getByRole("textbox");
+    expect(inputBox).toHaveAttribute("name", "username");
+  });
+  test("case 2", () => {
+    render(<App />);
+    let inputBox = screen.getByRole("textbox");
+    expect(inputBox).toHaveAttribute("name", "username");
+  });
+  test("case 3", () => {
+    render(<App />);
+    let inputBox = screen.getByRole("textbox");
+    expect(inputBox).toHaveAttribute("name", "username");
+  });
+});
+describe.only("API test case group", () => {
+  test("case 1", () => {
+    render(<App />);
+    let inputBox = screen.getByRole("textbox");
+    expect(inputBox).toHaveAttribute("name", "username");
+  });
+  test("case 2", () => {
+    render(<App />);
+    let inputBox = screen.getByRole("textbox");
+    expect(inputBox).toHaveAttribute("name", "username");
+  });
+  test("case 3", () => {
+    render(<App />);
+    let inputBox = screen.getByRole("textbox");
+    expect(inputBox).toHaveAttribute("name", "username");
+  });
 });
--- src/sum.test.js
+++ src/sum.test.js
@@ -3,3 +3,4 @@ import sumNumber from "./sum";
 test("for number", () => {
   expect(sumNumber(10, 20)).toBe(30);
 });
+
